ba7d27f3f784b9698ba4397a42956a280c2c29d4
amitpatel <amit.patel@vayana.com>
2024-08-07 13:05:49 +0530

    Add jazzmin

--- config/settings/base.py
+++ config/settings/base.py
@@ -69,6 +69,7 @@ DJANGO_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # "django.contrib.humanize", # Handy template tags
+    "jazzmin",
     "django.contrib.admin",
     "django.forms",
 ]
