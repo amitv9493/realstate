bb509d2453ade001cf2f8839423cb38031e46265
amitpatel <amit.patel@vayana.com>
2024-09-01 20:53:31 +0530

    Update the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,16 @@
-git pull
-docker compose build
-docker compose restart
+#!/bin/bash
+
+handle_error() {
+    echo "Error: $1"
+    exit 1
+}
+
+
+git pull || handle_error "Failed to pull latest changes from the repository"
+
+docker compose build || handle_error "Failed to build Docker container"
+
+docker compose up -d || handle_error "Failed to start containers"
+
+echo "Restarted Successfully"
+exit 0
