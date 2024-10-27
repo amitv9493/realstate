fd13932b95215ea0dcd4bc728dcde3e5c154b937
amitpatel <amit.patel@vayana.com>
2024-10-27 19:29:05 +0530

    Minor fixes

--- realstate_new/users/models.py
+++ realstate_new/users/models.py
@@ -52,7 +52,8 @@ class Rating(models.Model):
     rated_by = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         related_name="ratings_given",
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
+        null=True,
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
