e75b0bf53a1550e54127b67dfdfc59b57b09d551
amitv9493 <amitv9493@gmail.com>
2024-11-23 21:33:56 +0530

    minor changes

--- realstate_new/notification/fcm.py
+++ realstate_new/notification/fcm.py
@@ -52,7 +52,8 @@ class FCM:
 
         if resp.status_code == status.HTTP_200_OK:
             _logger.info("Message sent to Firebase for delivery")
-        if resp.status_code == status.HTTP_404_NOT_FOUND:
+
+        elif resp.status_code == status.HTTP_404_NOT_FOUND:
             _logger.info("FCM device is stale")
 
         else:
