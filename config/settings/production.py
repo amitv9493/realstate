d5f9a37e8dcfcfe6b628f28a6e69cf3d89bfc93e
amitv9493 <amitv9493@gmail.com>
2024-12-01 23:05:20 +0530

    update production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -10,7 +10,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["65.20.90.117"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", [])
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -77,14 +77,14 @@ STORAGES = {
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="realstate-new <noreply@65.20.90.117>",
+    default="no-reply@adoorable.com",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
 EMAIL_SUBJECT_PREFIX = env(
     "DJANGO_EMAIL_SUBJECT_PREFIX",
-    default="[realstate-new] ",
+    default="[realstate-new-errors] ",
 )
 
 # ADMIN
